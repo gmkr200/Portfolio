4e7d5878fcf543f720082ab28f46f64550198204
manikumar <gmkr200@gmail.com>
2024-10-21 22:02:37 -0400

    worling on it

--- src/Data/aboutData.js
+++ src/Data/aboutData.js
@@ -1,7 +1,7 @@
 import whoImage from '../assets/images/who.jpg';
 export const aboutData = {
     title: "Who I am",
-    para2: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
+    para2: "msnikumsrreddy gsjje react developer just started the carrier sx, UK.",
     para1: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. ",
     image:whoImage
 }
