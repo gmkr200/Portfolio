6f276bd9be9119a85980b6dfe4a120d278235abd
manikumar <gmkr200@gmail.com>
2024-10-24 00:07:18 -0400

    change of the idea to diaply the card component s

--- src/Components/Projects/projects.jsx
+++ src/Components/Projects/projects.jsx
@@ -1,63 +1,72 @@
 import React from "react";
 import Slider from "react-slick";
-import { Link } from "react-router-dom";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-// Dummy projects data (Replace with your actual projectsData)
+// Sample project data with GitHub links
 const projectsData = [
-    { id: 1, name: "Project 1", description: "Description 1", image: "/images/project1.jpg" },
-    { id: 2, name: "Project 2", description: "Description 2", image: "/images/project2.jpg" },
-    { id: 3, name: "Project 3", description: "Description 3", image: "/images/project3.jpg" },
-    { id: 4, name: "Project 4", description: "Description 4", image: "/images/project4.jpg" },
+    {
+        id: 1,
+        name: "Project 1",
+        description: "A description of Project 1",
+        image: "/images/project1.jpg",
+        githubLink: "https://github.com/user/project1",
+    },
+    {
+        id: 2,
+        name: "Project 2",
+        description: "A description of Project 2",
+        image: "/images/project2.jpg",
+        githubLink: "https://github.com/user/project2",
+    },
+    {
+        id: 3,
+        name: "Project 3",
+        description: "A description of Project 3",
+        image: "/images/project3.jpg",
+        githubLink: "https://github.com/user/project3",
+    },
 ];
 
 const Projects = () => {
     const settings = {
-        dots: true,
+        dots: false,
         infinite: true,
-        speed: 500,
+        speed: 800,
         slidesToShow: 3,
         slidesToScroll: 1,
         centerMode: true,
         centerPadding: "0px",
-        focusOnSelect: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    centerPadding: "20px",
-                },
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 1,
-                    centerPadding: "40px",
-                },
-            },
-        ],
+        autoplay: true,
+        autoplaySpeed: 2000,
     };
 
     return (
         <div className="projects-container" id="projects">
-            <Slider {...settings} className="projects-carousel">
+            <Slider {...settings}>
                 {projectsData.map((project) => (
-                    <div key={project.id} className="project-card">
-                        <img src={project.image} alt={project.name} className="project-image" />
-                        <div className="project-info">
-                            <h3>{project.name}</h3>
-                            <p>{project.description}</p>
+                    <div key={project.id} className="project-card-wrapper">
+                        <div className="project-card">
+                            <img
+                                src={project.image}
+                                alt={project.name}
+                                className="project-image"
+                            />
+                            <div className="project-info">
+                                <h3 className="project-title">{project.name}</h3>
+                                <a
+                                    href={project.githubLink}
+                                    className="github-link"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    View on GitHub
+                                </a>
+                            </div>
                         </div>
                     </div>
                 ))}
             </Slider>
-            {projectsData.length > 3 && (
-                <Link to="/projectsPage" className="see-all-button">
-                    See All Projects
-                </Link>
-            )}
         </div>
     );
 };
