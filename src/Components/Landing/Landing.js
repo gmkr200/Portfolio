d2c30380157cee36695d3fa69c3c8fe24c02ff50
manikumar <gmkr200@gmail.com>
2024-10-25 23:01:10 -0400

    before make the changes

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 import {
   FaLinkedin,
   FaGithub,
@@ -9,9 +8,9 @@ import {
 } from "react-icons/fa";
 import "./Landing.css"; // Your CSS styling
 import { socialsData } from "../../Data/socialsData"; // Social media links data
-import dp from "../../assets/images/dp.jpg"; // Ensure the image path is correct
 import Manikumarreddy from "../../assets/resume/Manikumarreddy.pdf";
-
+import { LandingData } from "../../Data/Landing";
+import parse from "html-react-parser";
 const Landing = () => {
   return (
     <div className="landing">
@@ -52,7 +51,7 @@ const Landing = () => {
 
         {/* Image Section */}
         <img
-          src={dp}
+          src={LandingData.dp}
           alt="Manikumarreddy Gajjela"
           className="landing--img"
           style={{ width: "380px", borderRadius: "50%" }}
@@ -62,23 +61,9 @@ const Landing = () => {
         <div className="landing--container-right">
           <div className="lcr--content" style={{ color: "#e0d9d9" }}>
             <h1>About Me</h1>
-            <p>
-              Hi, I'm <strong>Manikumarreddy Gajjela</strong>, a passionate
-              full-stack developer based in New York, NY. With over 4 years of
-              experience, I specialize in creating robust and scalable web
-              applications using modern technologies.
-            </p>
-            <p>
-              Currently, I work at <strong>Tetra Computing LLC</strong> as a
-              Full Stack Developer, developing high-performance applications
-              with <strong>React.js</strong>, <strong>Node.js</strong>,{" "}
-              <strong>Express.js</strong>, and <strong>AWS</strong>.
-            </p>
-            <p>
-              I hold a Bachelor's degree from{" "}
-              <strong>Osmania University</strong> and am pursuing a Master’s at{" "}
-              <strong>Pace University</strong>.
-            </p>
+            <p> {parse(LandingData.para1)}</p>
+            <p> {parse(LandingData.para2)}</p>
+            <p>{parse(LandingData.para3)} </p>
             {/* Buttons Section */}
             <div className="button-container">
               <a
---
+++ src/Data/Landing.js
@@ -0,0 +1,19 @@
+import dp from "../assets/images/dp.jpg";
+
+export const LandingData = {
+  dp: dp,
+  para1: `
+    Hi, I'm <strong>Manikumarreddy Gajjela</strong>, a passionate
+    full-stack developer based in New York, NY. With over 4 years of
+    experience, I specialize in creating robust and scalable web
+    applications using modern technologies.
+  `,
+  para2: `
+ Currently, I work at <strong>Tetra Computing LLC</strong> as a
+              Full Stack Developer, developing high-performance applications
+              with <strong>React.js</strong>, <strong>Node.js</strong>,
+              <strong>Express.js</strong>, and <strong>AWS</strong>.`,
+  para3: `I hold a Bachelor's degree from
+              <strong>Osmania University</strong> and am pursuing a Master’s at
+              <strong>Pace University</strong>.`,
+};
