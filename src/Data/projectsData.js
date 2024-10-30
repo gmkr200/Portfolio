fbe57a69a37ada337fbcf59e316eefada0b0a91d
manikumar <gmkr200@gmail.com>
2024-10-30 00:37:00 -0400

    before cleaning

--- src/Data/projectsData.js
+++ src/Data/projectsData.js
@@ -1,75 +1,49 @@
-import one from '../assets/images/bg.jpg'
-export const projectsData = [
-    {
-        id: 1,
-        name: 'Employee Management',
-        description: 'This project aims to do something i donot know what to do ',
-        tags: ['React', 'CSS', 'Material Ui'],
-        githubLink: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
-    },
-    {
-        id: 2,
-        name: 'Railway Tracking',
-        description: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-        tags: ['Django', 'Tailwind CSS'],
-        githubLink: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one   },
-    {
-        id: 3,
-        name: 'Weather App',
-        description: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-        tags: ['Django', 'CSS', 'Material Ui'],
-        githubLink: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
-    },
-    {
-        id: 4,
-        name: 'Android Patient Tracker',
-        description: 'This project involves the development of an Android application for viewing and managing patient data.',
-        tags: ['Flutter', 'Firebase'],
-        githubLink: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one    },
-    {
-        id: 5,
-        name: 'E-Commerce App',
-        description: 'A Simple E-commerce application',
-        tags: ['React Native', 'Firebase'],
-        githubLink: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one    },
-    {
-        id: 6,
-        name: 'Uber Lite',
-        description: 'Uber clone',
-        tags: ['Flutter'],
-        githubLink: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one   },
-    {
-        id: 7,
-        name: 'Stock Market App',
-        description: 'A simple stock market API app',
-        tags: ['React', 'Redux', 'Bootstrap'],
-        githubLink: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
-    },
-    {
-        id: 8,
-        name: 'Car Pooling System',
-        description: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-        tags: ['Flutter', 'React'],
-        githubLink: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-        image: one
-    },
-]
+import { BsWatch } from "react-icons/bs";
+import one from "../assets/images/1.png";
+import two from "../assets/images/2.png";
+import three from "../assets/images/3.png";
+import four from "../assets/images/4.png";
+import five from "../assets/images/5.png";
+import six from "../assets/images/6.png";
 
+export const projectsData = [
+  {
+    id: 1,
+    name: "Stay WEll",
+    githubLink: "https://github.com/PACE-Team3/Staywell",
+    image: one,
+  },
+  {
+    id: 2,
+    name: "Hydrate",
+    githubLink: "https://github.com/PACE-Team3/Staywell",
+    image: two,
+  },
+  {
+    id: 3,
+    name: "Notes-Todo",
+    githubLink: "https://github.com/gmkr200/manis-notes",
+    image: three,
+  },
+  {
+    id: 4,
+    name: "Currency Converter",
+    githubLink: "https://github.com/gmkr200/Currency-Converter",
+    image: four,
+  },
+  {
+    id: 5,
+    name: "Recipe App",
+    githubLink: "https://github.com/gmkr200/Recipe-back-end",
+    image: five,
+  },
+  {
+    id: 6,
+    name: "Timer",
+    githubLink: "https://github.com/gmkr200/Stopwatch",
+    image: six,
+  },
+];
 
 // Do not remove any fields.
 // Leave it blank instead as shown below
